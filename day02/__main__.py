6ba8c105b65771e4df4688e8365eff5122e0184e
chbndrhnns <chbndrhnns@users.noreply.github.com>
2021-12-02 13:04:51 +0100

    feat: Day 02 - Part 01

---
+++ day02/__main__.py
@@ -0,0 +1,125 @@
+from pathlib import Path
+from typing import List
+
+
+class Position:
+    available_moves = []
+
+    def __init__(self, horizontal=0, depth=0):
+        self._h = horizontal
+        self._d = depth
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__()
+        cls.available_moves.append(cls)
+
+    @property
+    def horizontal(self):
+        return self._h
+
+    @property
+    def depth(self):
+        return self._d
+
+    @property
+    def multiple(self):
+        return self.depth * self.horizontal
+
+    @classmethod
+    def parse_string(cls, val: str):
+        move, amount = cls._split_string(val.strip())
+        move_cls = cls._find_move_cls(move)
+        return move_cls(int(amount))
+
+    @staticmethod
+    def _split_string(val):
+        try:
+            step, amount = val.split(" ")
+            return step, amount
+        except ValueError:
+            raise ValueError("String needs two values, separated by space")
+
+    @classmethod
+    def _find_move_cls(cls, string: str):
+        try:
+            return [move for move in cls.available_moves if move.__string__ == string][
+                0
+            ]
+        except IndexError:
+            raise ValueError(f"Move '{string}' not supported")
+
+    def __add__(self, other):
+        if not isinstance(other, self.__class__):
+            raise NotImplementedError()
+
+        return self.__class__(
+            horizontal=self.horizontal + other.horizontal,
+            depth=self.depth + other.depth,
+        )
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(h={self.horizontal}, d={self.depth})"
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other: "Position"):
+        if (
+            isinstance(other, self.__class__)
+            and self.depth == other.depth
+            and self.horizontal == other.horizontal
+        ):
+            return True
+
+        return False
+
+
+class Forward(Position):
+
+    __string__ = "forward"
+
+    def __init__(self, horizontal=0):
+        super().__init__(horizontal=horizontal)
+
+
+class Down(Position):
+    __string__ = "down"
+
+    def __init__(self, depth=0):
+        super().__init__(depth=depth)
+
+
+class Up(Position):
+    __string__ = "up"
+
+    def __init__(self, depth=0):
+        super().__init__(depth=-depth)
+
+
+class Submarine:
+    def __init__(self):
+        self._position = Position()
+
+    @property
+    def position(self):
+        return self._position
+
+    def move(self, command: Position):
+        self._position += command
+
+    def move_multiple(self, commands: List[Position]):
+        for step in commands:
+            self.move(step)
+
+    @classmethod
+    def create(cls):
+        return cls()
+
+
+if __name__ == "__main__":
+    s = Submarine.create()
+    with open(Path(__file__).parent / "data.txt", "r") as f:
+        moves = [Position.parse_string(s) for s in f.readlines()]
+        s.move_multiple(moves)
+        print(s.position)
+        print(s.position.multiple)
---
+++ day02/test_main.py
@@ -0,0 +1,83 @@
+import pytest
+
+from day02.__main__ import Down, Forward, Position, Submarine, Up
+
+
+class TestPosition:
+    def test_eq(self):
+        assert Position() == Position()
+        assert Position() != Position(1, 0)
+        assert Position(1, 1) + Position(2, 3) == Position(3, 4)
+
+    def test_add(self):
+        assert Position() + Position() == Position()
+        assert Position(1, 0) + Position(-1, 0) == Position()
+
+
+class TestSteps:
+    def test_up_decreases_depth(self):
+        assert Position() + Up(1) == Position(0, -1)
+
+    def test_down_increases_depth(self):
+        assert Position() + Down(1) == Position(0, 1)
+
+    @pytest.mark.parametrize(
+        "step, expected",
+        [("forward 5", Forward(5)), ("up 0", Up(0)), ("down 5", Down(5))],
+    )
+    def test_from_string_ok(self, step, expected):
+        assert Position.parse_string(step) == expected
+
+    @pytest.mark.parametrize(
+        "step,expected_error",
+        [("f 5", "'f' not supported"), ("", "two"), ("down", "two")],
+    )
+    def test_from_string_error(self, step, expected_error):
+        with pytest.raises(ValueError) as err:
+            Position.parse_string(step)
+
+        assert expected_error in err.value.args[0]
+
+
+def test_initial_position_is_0():
+    s = Submarine.create()
+    assert s.position == Position(0, 0)
+
+
+def test_can_move_submarine():
+    s = Submarine.create()
+    s.move(Forward(1))
+    assert s.position == Position(1, 0)
+
+
+def test_can_move_multiple_steps__empty():
+    s = Submarine.create()
+    s.move_multiple([])
+    assert s.position == Position(0, 0)
+
+
+def test_can_move_multiple_steps__single():
+    s = Submarine.create()
+    s.move_multiple([Forward(4)])
+    assert s.position == Position(4, 0)
+
+
+def test_can_move_multiple_steps__two():
+    s = Submarine.create()
+    s.move_multiple([Forward(4)])
+    assert s.position == Position(4, 0)
+
+
+def test_example():
+    steps = [
+        Forward(5),
+        Down(5),
+        Forward(8),
+        Up(3),
+        Down(8),
+        Forward(2),
+    ]
+    s = Submarine.create()
+    s.move_multiple(steps)
+    assert s.position == Position(15, 10)
+    assert s.position.multiple == 150
